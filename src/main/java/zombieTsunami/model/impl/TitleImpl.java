ef0521dc78379c3fa30c2c07a2cf133d61518463
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-12 13:16:13 +0100

    implementation setImage method

--- src/main/java/zombieTsunami/model/impl/TitleImpl.java
+++ src/main/java/zombieTsunami/model/impl/TitleImpl.java
@@ -1,21 +1,29 @@
 package main.java.zombieTsunami.model.impl;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import main.java.zombieTsunami.model.api.Title;
 
 public class TitleImpl implements Title{
 
+    private BufferedImage image;
+    private boolean collision = false;
+
     @Override
     public void setImage(String string) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setImage'");
+        try {
+            this.image = ImageIO.read(getClass().getResourceAsStream(string));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public BufferedImage getImage() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getImage'");
+        return this.image;
     }
     
 }
