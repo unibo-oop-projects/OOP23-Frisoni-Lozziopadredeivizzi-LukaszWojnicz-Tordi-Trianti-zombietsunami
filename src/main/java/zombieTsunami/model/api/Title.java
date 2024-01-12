9dd3f807288ce34d2939967e5b3a57c9c546b82f
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-12 13:13:31 +0100

    create Title and TitleManager

---
+++ src/main/java/zombieTsunami/model/api/Title.java
@@ -0,0 +1,10 @@
+package main.java.zombieTsunami.model.api;
+
+import java.awt.image.BufferedImage;
+
+public interface Title {
+
+    void setImage(String string);
+
+    BufferedImage getImage();
+}
---
+++ src/main/java/zombieTsunami/model/impl/TitleManagerImpl.java
@@ -0,0 +1,7 @@
+package main.java.zombieTsunami.model.impl;
+
+import main.java.zombieTsunami.model.api.TitleManager;
+
+public class TitleManagerImpl implements TitleManager{
+    
+}
