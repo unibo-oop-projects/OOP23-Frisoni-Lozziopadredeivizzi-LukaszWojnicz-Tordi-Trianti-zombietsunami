bd658c719721fa081e9dfc5da5555ebf6d49f479
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-16 11:37:29 +0100

    setTileImage firts implementation

--- src/main/java/zombieTsunami/model/impl/TileManagerImpl.java
+++ src/main/java/zombieTsunami/model/impl/TileManagerImpl.java
@@ -8,11 +8,23 @@ import main.java.zombieTsunami.model.api.TileManager;
 public class TileManagerImpl implements TileManager{
 
     private final List<TileImpl> itemMap = new ArrayList<>();
+    private final List<List<Integer>> mapTileNum = new ArrayList<>();
+
+    public TileManagerImpl(){
+        setTitleImage();
+        loadMap();
+    }
 
     @Override
     public void setTitleImage() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setTitleImage'");
+        this.itemMap.add(new TileImpl());
+        this.itemMap.get(0).setImage("");
+
+        this.itemMap.add(new TileImpl());
+        this.itemMap.get(1).setImage("");
+
+        this.itemMap.add(new TileImpl());
+        this.itemMap.get(2).setImage("");
     }
 
     @Override
