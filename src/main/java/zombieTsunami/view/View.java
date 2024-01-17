9f5bf909f0638909495706b0a99969350df48d50
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-18 00:43:26 +0100

    start Menu configurated

--- src/main/java/zombieTsunami/view/View.java
+++ src/main/java/zombieTsunami/view/View.java
@@ -5,20 +5,30 @@ import javax.swing.JFrame;
 import main.java.zombieTsunami.view.mapView.impl.MapImpl;
 
 public class View {
-    
-    public static void start(){
+
+    public static void start() {
         final JFrame window = new JFrame("Zombie Tsunami");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
 
-        final MapImpl panel = new MapImpl();
-        window.add(panel);
+        final StartMenu startMenu = new StartMenu();
+        final MapImpl gamePanel = new MapImpl();
+
+        startMenu.enable();
+        window.add(startMenu);
+
+        startMenu.getStartButton().addActionListener(e -> {
+            window.remove(startMenu);
+            window.add(gamePanel);
+            window.revalidate();
+            window.repaint();
+            gamePanel.startGameThread();
+        });
         window.pack();
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
-        panel.startGameThread();
     }
-    
+
 }
