275243bd01bb0ebb2a23d188fcf352c72780426f
AlexFr3 <alexfrisoni2003@gmail.com>
2024-01-18 15:48:03 +0100

    add zombie folders

---
+++ src/main/java/zombieTsunami/model/zombieModel/impl/ZombieImpl.java
@@ -0,0 +1,13 @@
+package main.java.zombieTsunami.model.zombieModel.impl;
+
+import main.java.zombieTsunami.model.zombieModel.api.Zombie;
+
+public class ZombieImpl implements Zombie{
+
+    @Override
+    public Pair<Integer, Integer> getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+    
+}
