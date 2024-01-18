9a41612f37dda4f59cc99bdee2a063b88990efb4
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2024-01-18 15:48:03 +0100

    add zombie folders

---
+++ src/main/java/zombieTsunami/model/zombieModel/api/Zombie.java
@@ -0,0 +1,7 @@
+package main.java.zombieTsunami.model.zombieModel.api;
+
+import main.java.zombieTsunami.model.zombieModel.impl.Pair;
+
+public interface Zombie {
+    Pair<Integer, Integer> getPosition();
+}
