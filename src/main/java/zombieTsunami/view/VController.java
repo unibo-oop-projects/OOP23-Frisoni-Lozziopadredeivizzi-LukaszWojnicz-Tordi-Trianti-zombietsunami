584af59f624f492e519430010278293f4362ced9
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-26 18:58:54 +0100

    create VController

---
+++ src/main/java/zombieTsunami/view/VController.java
@@ -0,0 +1,16 @@
+package main.java.zombieTsunami.view;
+
+import main.java.zombieTsunami.api.Controller;
+
+public class VController {
+    
+    private Controller control;
+
+    public void set(final Controller c){
+        this.control=c;
+    }
+
+    public void getScreenWC(){
+        return this.control.
+    }
+}
