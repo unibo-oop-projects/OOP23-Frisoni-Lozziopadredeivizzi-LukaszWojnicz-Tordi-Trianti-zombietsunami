07fe3d15a0c051d2c95672cb83dd36894a15852e
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-26 19:07:01 +0100

    modified interface methods

--- src/main/java/zombieTsunami/view/VControllerImpl.java
+++ src/main/java/zombieTsunami/view/VControllerImpl.java
@@ -7,14 +7,17 @@ public class VControllerImpl implements VController{
     
     private Controller control;
 
+    @Override
     public void set(final Controller c){
         this.control=c;
     }
 
+    @Override
     public int getScreenWC(){
         return this.control.getScreenWidth();
     }
 
+    @Override
     public int getScreenHC(){
         return this.control.getScreenHigh();
     }
