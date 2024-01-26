2bcd33ba632e66f768c3751b43b5cbe22f07eb4a
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-26 19:07:01 +0100

    modified interface methods

--- src/main/java/zombieTsunami/view/api/VController.java
+++ src/main/java/zombieTsunami/view/api/VController.java
@@ -1,5 +1,13 @@
 package main.java.zombieTsunami.view.api;
 
+import main.java.zombieTsunami.api.Controller;
+
 public interface VController {
+
+    void set(Controller c);
+
+    int getScreenWC();
+
+    int getScreenHC();
     
 }
