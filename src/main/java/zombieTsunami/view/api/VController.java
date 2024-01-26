a9c009b6a584a5216950a8f606fb79e61138ce6e
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-26 19:20:01 +0100

    applied new VController method

--- src/main/java/zombieTsunami/view/api/VController.java
+++ src/main/java/zombieTsunami/view/api/VController.java
@@ -1,5 +1,8 @@
 package main.java.zombieTsunami.view.api;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import main.java.zombieTsunami.api.Controller;
 
 public interface VController {
@@ -9,5 +12,11 @@ public interface VController {
     int getScreenWC();
 
     int getScreenHC();
+
+    int getFPSC();
+
+    void updateZombieC();
+
+    void drowMapC(Graphics2D g2);
     
 }
