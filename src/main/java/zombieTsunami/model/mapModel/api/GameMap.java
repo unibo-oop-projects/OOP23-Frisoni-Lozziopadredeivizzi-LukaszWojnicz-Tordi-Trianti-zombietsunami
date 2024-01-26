0e5f56d68e258c6e60fcd66fd4854a41828c89e9
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-26 15:23:54 +0100

    reorganization map model

---
+++ src/main/java/zombieTsunami/model/mapModel/api/GameMap.java
@@ -0,0 +1,6 @@
+package main.java.zombieTsunami.model.mapModel.api;
+
+public interface GameMap {
+    
+    public void loadMap();
+}
---
+++ src/main/java/zombieTsunami/model/mapModel/impl/GameMapImpl.java
@@ -0,0 +1,13 @@
+package main.java.zombieTsunami.model.mapModel.impl;
+
+import main.java.zombieTsunami.model.mapModel.api.GameMap;
+
+public class GameMapImpl  implements GameMap{
+
+    @Override
+    public void loadMap() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadMap'");
+    }
+    
+}
