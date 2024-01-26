475d44f6aba7f20eff8a0e39b4379264ea2a11ff
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-26 16:15:51 +0100

    fixed: loadMap & javadoc

--- src/main/java/zombieTsunami/model/mapModel/api/GameMap.java
+++ src/main/java/zombieTsunami/model/mapModel/api/GameMap.java
@@ -1,6 +1,11 @@
 package main.java.zombieTsunami.model.mapModel.api;
 
+import java.util.List;
+
 public interface GameMap {
     
-    public void loadMap();
+    /**
+     * @return a List of Lists (Matrix alike) with the item's number of the map 
+     */
+    public List<List<Integer>> loadMap();
 }
--- src/main/java/zombieTsunami/model/mapModel/impl/GameMapImpl.java
+++ src/main/java/zombieTsunami/model/mapModel/impl/GameMapImpl.java
@@ -6,24 +6,29 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import main.java.zombieTsunami.api.MapData;
+import main.java.zombieTsunami.model.MapDataImpl;
 import main.java.zombieTsunami.model.mapModel.api.GameMap;
 
 public class GameMapImpl implements GameMap {
 
     private final String MAP1 = "maps/map01.txt";
-    private final String filePath = TileImpl.ROOT + TileImpl.RESOURCES + MAP1; 
+    private final String filePath = TileImpl.ROOT + TileImpl.RESOURCES + MAP1;
+    
+    private final MapData data = new MapDataImpl();
+    private final List<List<Integer>> mapTileNum = new ArrayList<>();
 
     @Override
-    public void loadMap() {
+    public List<List<Integer>> loadMap() {
         try {
             InputStream is = getClass().getResourceAsStream(filePath);
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-            for (int row = 0; row < this.gameController.getMaxScreenRow(); row++) {
+            for (int row = 0; row < this.data.getMaxScRow(); row++) {
                 final String line = br.readLine();
                 final List<Integer> list = new ArrayList<>();
 
-                for (int col = 0; col < this.gameController.getMaxScreenCol(); col++) {
+                for (int col = 0; col < this.data.getMaxScCol(); col++) {
                     final List<String> numbers = List.of(line.split(" "));
                     list.add(Integer.parseInt(numbers.get(col)));
                 }
@@ -36,6 +41,8 @@ public class GameMapImpl implements GameMap {
         } catch (Exception e) {
             e.getMessage();
         }
+        
+        return mapTileNum;
     }
 
 }
