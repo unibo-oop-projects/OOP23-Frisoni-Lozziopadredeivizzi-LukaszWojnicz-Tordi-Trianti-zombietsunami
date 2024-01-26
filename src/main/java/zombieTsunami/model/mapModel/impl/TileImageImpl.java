5ff0d4f2f678fc58c0bb5c0cd8bef8a034cb0bc4
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-26 17:04:43 +0100

    fixed: warnings

--- src/main/java/zombieTsunami/model/mapModel/impl/TileImageImpl.java
+++ src/main/java/zombieTsunami/model/mapModel/impl/TileImageImpl.java
@@ -1,9 +1,7 @@
 package main.java.zombieTsunami.model.mapModel.impl;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import main.java.zombieTsunami.model.mapModel.api.TileImage;
 
