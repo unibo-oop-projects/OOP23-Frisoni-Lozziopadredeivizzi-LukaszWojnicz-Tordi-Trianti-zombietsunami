deffb1ea9d56c1e1a750e15fa8db2bd2fe0775b0
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-26 18:41:25 +0100

    added getter method

--- src/main/java/zombieTsunami/controller/ControllerImpl.java
+++ src/main/java/zombieTsunami/controller/ControllerImpl.java
@@ -2,6 +2,7 @@ package main.java.zombieTsunami.controller;
 
 import main.java.zombieTsunami.api.Controller;
 import main.java.zombieTsunami.api.Model;
+import main.java.zombieTsunami.model.MapData;
 import main.java.zombieTsunami.model.ModelImpl;
 
 public class ControllerImpl implements Controller{
@@ -11,5 +12,29 @@ public class ControllerImpl implements Controller{
     public void setModel(){
         this.model=new ModelImpl();
         this.model.setController(this);
+    }
+
+    public int getScreenCol(){
+        return MapData.getMaxScCol();
+    }
+
+    public int getScreenRow(){
+        return MapData.getMaxScRow();
+    }
+
+    public int getTitleSize(){
+        return MapData.getTitSize();
+    }
+
+    public long getFPS(){
+        return MapData.getFPS(); 
+    }
+
+    public int getScreenWidth(){
+        return MapData.getScreenW();
+    }
+
+    public int getScreenHigh(){
+        return MapData.getScreenH();
     }
 }
