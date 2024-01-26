ad104ed9f231acebe57ef6304980d387eff601dc
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-26 19:02:40 +0100

    added method in Controller interface

--- src/main/java/zombieTsunami/api/Controller.java
+++ src/main/java/zombieTsunami/api/Controller.java
@@ -1,5 +1,25 @@
 package main.java.zombieTsunami.api;
 
+import java.awt.Graphics2D;
+
 public interface Controller {
+
+    void setModel();
+
+    int getScreenCol();
+
+    int getScreenRow();
+
+    int getTitleSize();
+
+    long getFPS();
+
+    int getScreenWidth();
+
+    int getScreenHigh();
+
+    void drowMap(Graphics2D g2);
+
+    void updateZombie();
     
 }
