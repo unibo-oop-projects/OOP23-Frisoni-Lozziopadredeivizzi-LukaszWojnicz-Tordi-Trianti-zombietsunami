4ef4ce05ffe1631f77da057f4de878c80fbad7be
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-01-27 21:13:13 +0100

    deleted zombie controller folder

--- src/main/java/zombieTsunami/controller/zombieController/api/ControllerKeyHandler.java
+++
@@ -1,5 +0,0 @@
-package main.java.zombieTsunami.controller.zombieController.api;
-
-public interface ControllerKeyHandler {
-    
-}
--- src/main/java/zombieTsunami/controller/zombieController/api/ControllerZombie.java
+++
@@ -1,9 +0,0 @@
-package main.java.zombieTsunami.controller.zombieController.api;
-
-import java.awt.Graphics2D;
-
-public interface ControllerZombie {
-    void drawZombie(Graphics2D g2);
-    void update();
-    void handleKeyPress();
-}
