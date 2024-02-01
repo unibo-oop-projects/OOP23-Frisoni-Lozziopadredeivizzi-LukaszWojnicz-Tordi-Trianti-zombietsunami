735d45005287d2646f9a461b637c198c3225b377
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-02-01 22:32:48 +0100

    modified GameMap loadMap method & javadoc

--- src/main/java/zombieTsunami/model/mapModel/api/GameMap.java
+++ src/main/java/zombieTsunami/model/mapModel/api/GameMap.java
@@ -3,14 +3,9 @@ package zombieTsunami.model.mapModel.api;
 import java.util.List;
 
 public interface GameMap {
-    
-    /**
-     * @return a List with the item's number of the map 
-     */
-    List<Integer> loadMap();
 
     /**
-     * @return the list of Integer with the item's nuber of the map
+     * @return the list of Integer with the item's number of the map
      */
     List<Integer> getLoadedMapList();
 }
