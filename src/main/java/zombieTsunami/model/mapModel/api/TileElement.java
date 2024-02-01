e308fa58b427cdb493d2330d2975f397b3c73309
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-02-01 22:50:40 +0100

    add getTileElement javadoc

--- src/main/java/zombieTsunami/model/mapModel/api/TileElement.java
+++ src/main/java/zombieTsunami/model/mapModel/api/TileElement.java
@@ -4,5 +4,9 @@ import java.util.List;
 
 public interface TileElement {
 
+    /**
+     * @return a List of strings, where the strings are the file's name of the
+     *         tiles, putten into their respective index.
+     */
     List<String> getTileElement();
 }
