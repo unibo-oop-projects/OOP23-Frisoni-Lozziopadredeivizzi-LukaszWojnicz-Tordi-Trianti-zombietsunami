a8b283e181eb95c08e0e50dad0a4b59e35e7a5c0
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-02-01 22:42:43 +0100

    add javadoc fot getScreenTilePosition

--- src/main/java/zombieTsunami/model/mapModel/api/MapPosList.java
+++ src/main/java/zombieTsunami/model/mapModel/api/MapPosList.java
@@ -6,8 +6,19 @@ import zombieTsunami.api.Pair;
 
 public interface MapPosList {
 
+    /**
+     * @param worldRow      the map's (world) row
+     * @param worldCol      the map's (world) col
+     * @param titleSize     the size of title
+     * @param zombieWorldX  the zombie's X coordinate on the map (world)
+     * @param zombieWorldY  the zombie's Y coordinate on the map (world)
+     * @param zombieScreenX the zombie's X coordinate on the screen
+     * @param zombieScreenY the zombie's Y coordinate on the screen
+     * @return a List of Pair of Integers, where the pair are the positions of every
+     *         single tile that has to be putten into the graphic.
+     */
     List<Pair<Integer, Integer>> getScreenTilePosition(int worldRow, int worldCol, int titleSize,
             final int zombieWorldX, final int zombieWorldY, final int zombieScreenX,
             final int zombieScreenY);
-        
+
 }
