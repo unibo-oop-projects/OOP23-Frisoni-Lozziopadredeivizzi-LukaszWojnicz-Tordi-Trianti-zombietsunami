fe89ebd1756986ea18f9a016464efd06cb1bbc08
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-02-01 23:48:37 +0100

    added setSingleTile javadoc

--- src/main/java/zombieTsunami/view/MapView/impl/TileImageImpl.java
+++ src/main/java/zombieTsunami/view/MapView/impl/TileImageImpl.java
@@ -14,13 +14,20 @@ public class TileImageImpl implements TileImage {
 
     @Override
     public List<TileImpl> setTileImage(final List<String> element) {
-        for(int i = 0; i<element.size(); i++){
+        for (int i = 0; i < element.size(); i++) {
             setSingleTile(i, element.get(i));
         }
         return itemMap;
 
     }
 
+    /**
+     * This method sets at the @param index of the list a new Tile with a new
+     * BufferedImage.
+     * 
+     * @param index  the index of the List, which match the value of the tile
+     * @param source the image file source name
+     */
     private final void setSingleTile(final int index, final String source) {
         this.itemMap.add(index, new TileImpl());
         this.itemMap.get(index).setImage(ROOT + source);
