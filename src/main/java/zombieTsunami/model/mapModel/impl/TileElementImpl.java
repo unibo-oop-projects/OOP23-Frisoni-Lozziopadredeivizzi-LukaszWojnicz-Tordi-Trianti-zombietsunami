4152f98b53973a97e9c0bb626fcf602b6203c9b9
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-02-02 15:38:20 +0100

    chore: changes in TileElementImpl to pass the checkstyle test

--- src/main/java/zombieTsunami/model/mapModel/impl/TileElementImpl.java
+++ src/main/java/zombieTsunami/model/mapModel/impl/TileElementImpl.java
@@ -5,20 +5,20 @@ import java.util.List;
 
 import zombieTsunami.model.mapModel.api.TileElement;
 
-public class TileElementImpl implements TileElement {
+public final class TileElementImpl implements TileElement {
 
     private final List<String> element;
 
-    private final String DIRT = "dirt.png";
-    private final String SKY = "sky.png";
-    private final String STREET = "street.png";
-    private final String BULDING_LEFT = "buldingLeft.png";
-    private final String BULDING_NORTH_LEFT = "buldingNorthLeft.png";
-    private final String BULDING_NORTH = "buldingNorth.png";
-    private final String BULDING_NORTH_RIGHT = "buldingNorthRight.png";
-    private final String BULDING_RIGHT = "buldingRight.png";
-    private final String BULDING_DOOR = "buldingDoor.png";
-    private final String BULDING_WINDOW = "buldingWindow.png";
+    private static final String DIRT = "dirt.png";
+    private static final String SKY = "sky.png";
+    private static final String STREET = "street.png";
+    private static final String BULDING_LEFT = "buldingLeft.png";
+    private static final String BULDING_NORTH_LEFT = "buldingNorthLeft.png";
+    private static final String BULDING_NORTH = "buldingNorth.png";
+    private static final String BULDING_NORTH_RIGHT = "buldingNorthRight.png";
+    private static final String BULDING_RIGHT = "buldingRight.png";
+    private static final String BULDING_DOOR = "buldingDoor.png";
+    private static final String BULDING_WINDOW = "buldingWindow.png";
 
     public TileElementImpl() {
         this.element = new ArrayList<>();
@@ -30,17 +30,18 @@ public class TileElementImpl implements TileElement {
      * file, and puts them into their respective index, which are the number values
      * that represent that tile into the map.
      */
-    private final void setTileElement() {
-        this.element.add(0, DIRT);
-        this.element.add(1, STREET);
-        this.element.add(2, SKY);
-        this.element.add(3, BULDING_LEFT);
-        this.element.add(4, BULDING_NORTH_LEFT);
-        this.element.add(5, BULDING_NORTH);
-        this.element.add(6, BULDING_NORTH_RIGHT);
-        this.element.add(7, BULDING_RIGHT);
-        this.element.add(8, BULDING_DOOR);
-        this.element.add(9, BULDING_WINDOW);
+    private void setTileElement() {
+        int pos = 0;
+        this.element.add(pos++, DIRT);
+        this.element.add(pos++, STREET);
+        this.element.add(pos++, SKY);
+        this.element.add(pos++, BULDING_LEFT);
+        this.element.add(pos++, BULDING_NORTH_LEFT);
+        this.element.add(pos++, BULDING_NORTH);
+        this.element.add(pos++, BULDING_NORTH_RIGHT);
+        this.element.add(pos++, BULDING_RIGHT);
+        this.element.add(pos++, BULDING_DOOR);
+        this.element.add(pos++, BULDING_WINDOW);
     }
 
     @Override
