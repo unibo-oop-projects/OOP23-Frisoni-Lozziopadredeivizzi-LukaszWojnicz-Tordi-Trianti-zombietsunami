df95328b08593be6830ff898b109b462840915b3
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-02-02 14:13:26 +0100

    chore: changes in MapData class to pass the checkstyle test

--- src/main/java/zombieTsunami/model/MapData.java
+++ src/main/java/zombieTsunami/model/MapData.java
@@ -1,26 +1,34 @@
 package zombieTsunami.model;
 
-public class MapData {
+/**
+ * This an Utility class, used for store the informations about the screen
+ * dimensions, world dimensions and FPS of the game. Its role is to give those
+ * informations at the elements who needs them.
+ */
+public final class MapData {
 
     // SCREEN SETTINGS
-    private final static int ORIGINAL_TITLE_SIZE = 16;
-    private final static int SCALE = 3;
-    private final static int TITLE_SIZE = ORIGINAL_TITLE_SIZE * SCALE;
+    private static final int ORIGINAL_TITLE_SIZE = 16;
+    private static final int SCALE = 3;
+    private static final int TITLE_SIZE = ORIGINAL_TITLE_SIZE * SCALE;
 
-    private final static int MAX_SCREEN_COL = 16;
-    private final static int MAX_SCREEN_ROW = 9;
-    private final static int SCREEN_WIDTH = TITLE_SIZE * MAX_SCREEN_COL;
-    private final static int SCREEN_HIGHT = TITLE_SIZE * MAX_SCREEN_ROW;
+    private static final int MAX_SCREEN_COL = 16;
+    private static final int MAX_SCREEN_ROW = 9;
+    private static final int SCREEN_WIDTH = TITLE_SIZE * MAX_SCREEN_COL;
+    private static final int SCREEN_HIGHT = TITLE_SIZE * MAX_SCREEN_ROW;
 
-    //FPS
-    private final static int FPS = 60;
+    // FPS
+    private static final int FPS = 60;
 
-    //WORLD SETTINGS
-    private final static int MAX_WORLD_COL = 50;
-    private final static int MAX_WORLD_ROW = 12;
-    private final static int WORLD_WIDTH = TITLE_SIZE * MAX_WORLD_COL;
-    private final static int WORLD_HIGHT = TITLE_SIZE * MAX_WORLD_ROW;
+    // WORLD SETTINGS
+    private static final int MAX_WORLD_COL = 50;
+    private static final int MAX_WORLD_ROW = 12;
+    private static final int WORLD_WIDTH = TITLE_SIZE * MAX_WORLD_COL;
+    private static final int WORLD_HIGHT = TITLE_SIZE * MAX_WORLD_ROW;
 
+    private MapData() {
+
+    }
 
     /**
      * @return the screen's cols
@@ -67,28 +75,28 @@ public class MapData {
     /**
      * @return the map's (world) cols
      */
-    public static int getMaxWorldCol(){
+    public static int getMaxWorldCol() {
         return MAX_WORLD_COL;
     }
 
     /**
      * @return the map's (world) rows
      */
-    public static int getMaxWorldRow(){
+    public static int getMaxWorldRow() {
         return MAX_WORLD_ROW;
     }
 
     /**
      * @return the map (world) width
      */
-    public static int getWorldWidth(){
+    public static int getWorldWidth() {
         return WORLD_WIDTH;
     }
 
     /**
      * @return the map (world) height
      */
-    public static int getWorldHeight(){
+    public static int getWorldHeight() {
         return WORLD_HIGHT;
     }
 
