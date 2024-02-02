458da1e4ed2e7d26cf932dc6cc4cefa8277df490
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-02-02 01:08:23 +0100

    chore: changes to pass the checksyle tests

--- src/main/java/zombieTsunami/api/Pair.java
+++ src/main/java/zombieTsunami/api/Pair.java
@@ -1,64 +1,83 @@
 package zombieTsunami.api;
-/*
- * A standard generic Pair<X,Y>, with getters, hashCode, equals, and toString well implemented. 
+/**
+ * A standard generic Pair<X,Y>, with getters, hashCode, equals, and toString well implemented.
+ * 
+ * @param <X> is the type of the first element.
+ * @param <Y> is the type of the secoond element.
  */
+public final class Pair<X, Y> {
 
-public final class Pair<X,Y> {
-	
-	private final X x;
-	private final Y y;
-	
-	public Pair(X x, Y y) {
-		super();
-		this.x = x;
-		this.y = y;
-	}
+    private final X x;
+    private final Y y;
 
-	public X getX() {
-		return x;
-	}
+    /**
+     * Contructor of the class, creates a new Pair and sets the given parameters.
+     * 
+     * @param x is the first element.
+     * @param y is the second element.
+     */
+    public Pair(final X x, final Y y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
 
-	public Y getY() {
-		return y;
-	}
+    /**
+     * @return the first element of the Pair.
+     */
+    public X getX() {
+        return x;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((x == null) ? 0 : x.hashCode());
-		result = prime * result + ((y == null) ? 0 : y.hashCode());
-		return result;
-	}
+    /**
+     * @return the second element of the Pair.
+     */
+    public Y getY() {
+        return y;
+    }
 
-	@SuppressWarnings("rawtypes")
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Pair other = (Pair) obj;
-		if (x == null) {
-			if (other.x != null)
-				return false;
-		} else if (!x.equals(other.x))
-			return false;
-		if (y == null) {
-			if (other.y != null)
-				return false;
-		} else if (!y.equals(other.y))
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((x == null) ? 0 : x.hashCode());
+        result = prime * result + ((y == null) ? 0 : y.hashCode());
+        return result;
+    }
 
-	@Override
-	public String toString() {
-		return "Pair [x=" + x + ", y=" + y + "]";
-	}
-	
-	
+    @SuppressWarnings("rawtypes")
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Pair other = (Pair) obj;
+        if (x == null) {
+            if (other.x != null) {
+                return false;
+            }
+        } else if (!x.equals(other.x)) {
+            return false;
+        }
+        if (y == null) {
+            if (other.y != null) {
+                return false;
+            }
+        } else if (!y.equals(other.y)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair [x=" + x + ", y=" + y + "]";
+    }
 
 }
