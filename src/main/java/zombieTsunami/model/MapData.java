6d48c81a0a25a73ef69ee0a83104c2e6f1f36c8b
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-02-02 13:50:16 +0100

    chore: changed fild's name for checkstyle test

--- src/main/java/zombieTsunami/model/MapData.java
+++ src/main/java/zombieTsunami/model/MapData.java
@@ -3,44 +3,44 @@ package zombieTsunami.model;
 public class MapData {
 
     // SCREEN SETTINGS
-    private final static int originalTitleSize = 16;
-    private final static int scale = 3;
-    private final static int titleSize = originalTitleSize * scale;
+    private final static int ORIGINAL_TITLE_SIZE = 16;
+    private final static int SCALE = 3;
+    private final static int TITLE_SIZE = ORIGINAL_TITLE_SIZE * SCALE;
 
-    private final static int maxScreenCol = 16;
-    private final static int maxScreenRow = 9;
-    private final static int screenWidth = titleSize * maxScreenCol;
-    private final static int screenHigh = titleSize * maxScreenRow;
+    private final static int MAX_SCREEN_COL = 16;
+    private final static int MAX_SCREEN_ROW = 9;
+    private final static int SCREEN_WIDTH = TITLE_SIZE * MAX_SCREEN_COL;
+    private final static int SCREEN_HIGHT = TITLE_SIZE * MAX_SCREEN_ROW;
 
     //FPS
     private final static int FPS = 60;
 
     //WORLD SETTINGS
-    private final static int maxWorldCol = 50;
-    private final static int maxWorldRow = 12;
-    private final static int worldWidth = titleSize * maxWorldCol;
-    private final static int worldHight = titleSize * maxWorldRow;
+    private final static int MAX_WORLD_COL = 50;
+    private final static int MAX_WORLD_ROW = 12;
+    private final static int WORLD_WIDTH = TITLE_SIZE * MAX_WORLD_COL;
+    private final static int WORLD_HIGHT = TITLE_SIZE * MAX_WORLD_ROW;
 
 
     /**
      * @return the screen's cols
      */
     public static int getMaxScCol() {
-        return maxScreenCol;
+        return MAX_SCREEN_COL;
     }
 
     /**
      * @return the screen's rows
      */
     public static int getMaxScRow() {
-        return maxScreenRow;
+        return MAX_SCREEN_ROW;
     }
 
     /**
      * @return the title's size
      */
     public static int getTitSize() {
-        return titleSize;
+        return TITLE_SIZE;
     }
 
     /**
@@ -54,42 +54,42 @@ public class MapData {
      * @return the screen width
      */
     public static int getScreenW() {
-        return screenWidth;
+        return SCREEN_WIDTH;
     }
 
     /**
      * @return the screen height
      */
     public static int getScreenH() {
-        return screenHigh;
+        return SCREEN_HIGHT;
     }
 
     /**
      * @return the map's (world) cols
      */
     public static int getMaxWorldCol(){
-        return maxWorldCol;
+        return MAX_WORLD_COL;
     }
 
     /**
      * @return the map's (world) rows
      */
     public static int getMaxWorldRow(){
-        return maxWorldRow;
+        return MAX_WORLD_ROW;
     }
 
     /**
      * @return the map (world) width
      */
     public static int getWorldWidth(){
-        return worldWidth;
+        return WORLD_WIDTH;
     }
 
     /**
      * @return the map (world) height
      */
     public static int getWorldHeight(){
-        return worldHight;
+        return WORLD_HIGHT;
     }
 
 }
