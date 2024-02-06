ee554dc30e96618d7bfb12a64dc5fc5becabd573
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-02-07 00:00:40 +0100

    added javadocs to StartMenu class

--- src/main/java/zombietsunami/view/StartMenu.java
+++ src/main/java/zombietsunami/view/StartMenu.java
@@ -11,7 +11,9 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.plaf.DimensionUIResource;
-
+/**
+ * This class is the start menu panel for the game.
+ */
 public final class StartMenu extends JPanel {
 
     private static final int FONT_SIZE = 50;
@@ -22,12 +24,20 @@ public final class StartMenu extends JPanel {
     private final JButton rulesButton = new JButton("RULES");
     private final JLabel label = new JLabel("Zombie Tsunami");
 
+    /**
+     * Sets the panel dimensions.
+     * @param screenW the screen width 
+     * @param screenH the screen hight
+     */
     public StartMenu(final int screenW, final int screenH) {
         this.setBackground(Color.lightGray);
         this.setPreferredSize(new DimensionUIResource(screenW, screenH));
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
     }
 
+    /**
+     * This method enable the start panel label.
+     */
     private void enableLabel() {
         this.label.setFont(new Font("Ariel", Font.BOLD, FONT_SIZE));
         this.add(Box.createVerticalStrut(LABEL_HEIGHT));
@@ -35,12 +45,18 @@ public final class StartMenu extends JPanel {
         this.add(label);
     }
 
+    /**
+     * This method enable the start panlel 'start game' button.
+     */
     private void enableStartButton() {
         this.add(Box.createVerticalStrut(BUTTONS_HEIGHT));
         this.startButtonGame.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.add(startButtonGame);
     }
 
+    /**
+     * This method enable the start panel 'rules' button.
+     */
     private void enableRulesButton() {
         this.add(Box.createVerticalStrut(BUTTONS_HEIGHT));
         this.rulesButton.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -48,18 +64,27 @@ public final class StartMenu extends JPanel {
         addActionListenerToRuleButton();
     }
 
+    /**
+     * This method adds an action listener to the 'rules' button. 
+     */
     private void addActionListenerToRuleButton() {
         this.rulesButton.addActionListener(e -> {
             JOptionPane.showMessageDialog(null, "regole...", "Regole", JOptionPane.INFORMATION_MESSAGE);
         });
     }
 
+    /**
+     * This method enable all the start panel elements.
+     */
     public void enable() {
         enableLabel();
         enableStartButton();
         enableRulesButton();
     }
 
+    /**
+     * @return the 'start game' button
+     */
     public JButton getStartButton() {
         return this.startButtonGame;
     }
