adac122fddf679aa35a62ee1bc40ff611786b6f5
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-02-07 00:44:38 +0100

    added javadocs to View class and made it final

--- src/main/java/zombietsunami/view/View.java
+++ src/main/java/zombietsunami/view/View.java
@@ -8,18 +8,30 @@ import zombietsunami.view.api.KeyHandler;
 import zombietsunami.view.api.VController;
 import zombietsunami.view.mapView.impl.MapImpl;
 
-public class View {
+/**
+ * This utility class is the main view of the game and sets the main JFrame.
+ */
+public final class View {
 
+    private View() {
+    }
 
+    /**
+     * This method sets the main frame and puts into it the various panels.
+     * 
+     * @param c      sets the view controller of the class
+     * @param width  is the screen width
+     * @param height is the screen height
+     */
     public static void start(final VController c, final int width, final int height) {
         final JFrame window = new JFrame("Zombie Tsunami");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setFocusable(true);
-        final KeyHandler keyH= new KeyHandlerImpl();
+        final KeyHandler keyH = new KeyHandlerImpl();
         window.addKeyListener((KeyListener) keyH);
         final StartMenu startMenu = new StartMenu(width, height);
-        final MapImpl gamePanel = new MapImpl(c,keyH);
+        final MapImpl gamePanel = new MapImpl(c, keyH);
 
         startMenu.enable();
         window.add(startMenu);
