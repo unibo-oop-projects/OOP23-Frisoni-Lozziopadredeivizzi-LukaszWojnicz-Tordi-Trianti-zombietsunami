438647077ce318b31053ec25c6fcacdf606696a0
Luca Trianti <150790921+Lukottino@users.noreply.github.com>
2024-02-07 18:27:41 +0100

    Added Javadoc to the some classes
    Classes:
    -Bomb.java
    -ObstacleEntity.java
    -DrawBomb.java
    -DrawBreakable.java

--- src/main/java/zombietsunami/model/obstacleModel/impl/ObstacleEntity.java
+++ src/main/java/zombietsunami/model/obstacleModel/impl/ObstacleEntity.java
@@ -1,22 +1,41 @@
 package zombietsunami.model.obstacleModel.impl;
 
-public class ObstacleEntity{
+/**
+ * Defines the entity object of an Obstacle.
+ */
+public class ObstacleEntity {
 
     private int x, y;
 
+    /**
+     * Gets the X coordinate of the Entity.
+     * @return the X coordinate of the Entity
+    */
     public int getX() {
         return x;
     }
 
-    public void setX(int x) {
+    /**
+     * Sets the X coordinate of the Entity.
+     * @param x the X coordinate that should be set.
+    */
+    public void setX(final int x) {
         this.x = x;
     }
 
+    /**
+     * Gets the Y coordinate of the Entity.
+     * @return the Y coordinate of the Entity
+    */
     public int getY() {
         return y;
     }
 
-    public void setY(int y) {
+    /**
+     * Sets the Y coordinate of the Entity.
+     * @param y the Y coordinate that should be set.
+    */
+    public void setY(final int y) {
         this.y = y;
     }
 
--- src/main/java/zombietsunami/view/obstacleView/api/DrawBomb.java
+++ src/main/java/zombietsunami/view/obstacleView/api/DrawBomb.java
@@ -5,7 +5,22 @@ import java.awt.image.BufferedImage;
 
 import zombietsunami.view.api.VController;
 
+/**
+ * Interface representing the drawing and updating of the bomb.
+ */
 public interface DrawBomb {
-    void drawBombV(final Graphics2D g2, final VController controller);
+    /**
+     * Draws the bomb on the graphics context based on the same controller.
+     * 
+     * @param g2         The graphics context.
+     * @param controller The controller with the game-related information.
+     */
+    void drawBombV(Graphics2D g2, VController controller);
+
+    /**
+     * Gets the image representation of the bomb.
+     * 
+     * @return The BufferedImage representing the bomb.
+     */
     BufferedImage getBomb();
 }
