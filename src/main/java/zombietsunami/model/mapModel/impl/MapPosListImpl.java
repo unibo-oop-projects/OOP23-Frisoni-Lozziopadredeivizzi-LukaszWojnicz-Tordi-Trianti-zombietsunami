4774535be1534f7ac0e344e5124ffe8b9932f908
Lorenzo Tordi <lorenzo.tordi2@studio.unibo.it>
2024-02-07 13:18:07 +0100

    improved map camera & tile drawing

--- src/main/java/zombietsunami/model/mapModel/impl/MapPosListImpl.java
+++ src/main/java/zombietsunami/model/mapModel/impl/MapPosListImpl.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import zombietsunami.api.Pair;
 import zombietsunami.model.mapModel.api.MapPosList;
+
 /**
- * This class implements the MapPosList interface {@link zombietsunami.model.mapModel.api.MapPosList}.
+ * This class implements the MapPosList interface
+ * {@link zombietsunami.model.mapModel.api.MapPosList}.
  */
 public final class MapPosListImpl implements MapPosList {
 
@@ -21,11 +23,20 @@ public final class MapPosListImpl implements MapPosList {
 
         for (int worldRow = 0; worldRow < maxWorldRow; worldRow++) {
             for (int worldCol = 0; worldCol < maxWorldCol; worldCol++) {
+
                 worldX = worldCol * titleSize;
                 worldY = worldRow * titleSize;
                 final int screenX = worldX - zombieWorldX + zombieScreenX;
                 final int screenY = worldY - zombieWorldY + zombieScreenY;
-                screenTilePos.add(pos, new Pair<Integer, Integer>(screenX, screenY));
+
+                if (worldX + titleSize > zombieWorldX - zombieScreenX
+                        && worldX - titleSize < zombieWorldX + zombieScreenX
+                        && worldY + titleSize > zombieWorldY - zombieScreenY
+                        && worldY < zombieWorldY + zombieScreenY) {
+                    screenTilePos.add(pos, new Pair<Integer, Integer>(screenX, screenY));
+                } else {
+                    screenTilePos.add(pos, null);
+                }
                 pos++;
             }
         }
