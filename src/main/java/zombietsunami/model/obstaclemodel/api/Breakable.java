78607cb5d1460caddb9b2b89900b6f622bc88f4a
Luca Trianti <150790921+Lukottino@users.noreply.github.com>
2024-02-08 15:24:12 +0100

    Added javadoc to Breakable and Obstacle

--- src/main/java/zombietsunami/model/obstaclemodel/api/Breakable.java
+++ src/main/java/zombietsunami/model/obstaclemodel/api/Breakable.java
@@ -1,6 +1,20 @@
 package zombietsunami.model.obstaclemodel.api;
 
+/**
+ * Defines the Breakable object.
+ */
 public interface Breakable {
-    public boolean canBreakObstacle(int zombieForce);
-    public void breakObstacle();
+
+    /**
+     * Checks if the zombie can break the obstacle.
+     * @return true if the zombies force is greater than the obstacle one
+     * false otherwise.
+     * @param zombieForce the force of the zombie.
+     */
+    boolean canBreakObstacle(int zombieForce);
+
+    /**
+     * Method that breaks the obstacle.
+     */
+    void breakObstacle();
 }
--- src/main/java/zombietsunami/model/obstaclemodel/api/Obstacle.java
+++ src/main/java/zombietsunami/model/obstaclemodel/api/Obstacle.java
@@ -1,5 +1,12 @@
 package zombietsunami.model.obstaclemodel.api;
 
+/**
+ * Determines the Obstacle object.
+ */
 public interface Obstacle {
+
+    /**
+     * Updates the obstacle.
+     */
     void update();
 }
