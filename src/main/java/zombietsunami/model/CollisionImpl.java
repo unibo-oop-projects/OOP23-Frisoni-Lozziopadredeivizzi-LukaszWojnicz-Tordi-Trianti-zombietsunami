9c8b40d46dba4081544f0bf5738171cd0257205e
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2024-02-09 17:32:58 +0100

    add Collision file

---
+++ src/main/java/zombietsunami/model/CollisionImpl.java
@@ -0,0 +1,10 @@
+package zombietsunami.model;
+
+import zombietsunami.api.Collision;
+public class CollisionImpl implements Collision{
+
+    @Override
+    public void collisionZombieObstacle() {
+    }
+    
+}
