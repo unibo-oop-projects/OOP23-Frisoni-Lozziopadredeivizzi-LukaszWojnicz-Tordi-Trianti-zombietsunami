a9dc7f7621e4493f66c4713fbcf0765fd35c5abb
Luca Trianti <150790921+Lukottino@users.noreply.github.com>
2024-02-11 16:10:09 +0100

    Fixed crash and fixed Thresholds

--- src/main/java/zombietsunami/model/CollisionImpl.java
+++ src/main/java/zombietsunami/model/CollisionImpl.java
@@ -3,17 +3,14 @@ package zombietsunami.model;
 import java.util.List;
 
 import zombietsunami.model.zombiemodel.api.Zombie;
-import zombietsunami.model.zombiemodel.impl.ZombieImpl;
 import zombietsunami.api.Collision;
 import zombietsunami.model.mapmodel.api.GameMap;
 import zombietsunami.model.obstaclemodel.api.Bomb;
 import zombietsunami.model.obstaclemodel.api.Breakable;
 public class CollisionImpl implements Collision{
 
-    private static final int THRESHOLD_1 = 80;
-    private static final int THRESHOLD_2 = 100;
-
-    private boolean collision = false;
+    private static final int THRESHOLD_1 = 60;
+    private static final int THRESHOLD_2 = 104;
 
     @Override
     public void collisionZombieObstacle(final List<Bomb> bombList, final List<Breakable> breakableList, final int tileSize, final Zombie zombie, final GameMap gameMap) {
@@ -22,7 +19,7 @@ public class CollisionImpl implements Collision{
                 if(bombList.get(i) != null) {
                     if(bombList.get(i).getX() > THRESHOLD_1 &&
                         bombList.get(i).getX() < THRESHOLD_2 &&
-                        zombie.getScreenY() > bombList.get(i).getY() &&
+                        zombie.getScreenY() > bombList.get(i).getY() - tileSize &&
                         zombie.getScreenY() < bombList.get(i).getY() + tileSize) {
                             gameMap.removeObstacleListItem(i);
                             bombList.set(i, null);
@@ -36,7 +33,7 @@ public class CollisionImpl implements Collision{
                 if(breakableList.get(i) != null) {
                     if(breakableList.get(i).getX() > THRESHOLD_1 &&
                         breakableList.get(i).getX() < THRESHOLD_2 &&
-                        zombie.getScreenY() > breakableList.get(i).getY() &&
+                        zombie.getScreenY() > breakableList.get(i).getY() - tileSize &&
                         zombie.getScreenY() < breakableList.get(i).getY() + tileSize) {
                             gameMap.removeObstacleListItem(i);
                             breakableList.set(i, null);
