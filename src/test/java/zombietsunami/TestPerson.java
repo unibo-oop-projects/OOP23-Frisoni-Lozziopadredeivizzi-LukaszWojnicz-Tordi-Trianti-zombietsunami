bdc7aceb4a63c79a7ef0b2b0248be1638aef28a1
LukaszWojnicz <lukasz.wojnicz@studio.unibo.it>
2024-02-17 00:50:25 +0100

    Ricreation of TestPerson for problems

--- src/test/java/zombietsunami/TestPerson.java
+++ src/test/java/zombietsunami/TestPerson.java
@@ -9,19 +9,17 @@ import zombietsunami.model.zombiemodel.impl.ZombieImpl;
 /**
  * Test class for Person.
  * 
- * This class tests the funcionalities of the Person class.
+ * This class test the increase of the Person.
  */
 class TestPerson {
     private final Zombie zombie = new ZombieImpl();
 
     /**
-     * Checks if the increase has been dealt correctly to the zombie.
+     * Checks if the increase has been dealt correctly to the Zombie.
      */
     @Test
-    void checkIncrease(){
+    void checkIncrease() {
         zombie.increaseStrength();
         assertEquals(zombie.getStrength(), 2);
-
     }
-    
 }
